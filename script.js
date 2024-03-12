a37945943612125455f1f90a8ee7bd8c6761b0ac
manjeet <manjeet7171gautam@gmail.com>
2024-03-13 00:30:12 +0530

    Added score board and next btn working properly now.

--- script.js
+++ script.js
@@ -1,59 +1,98 @@
-let arr = [["National Animal of India?", "Lion", "Tiger", "Horse", "Deer", "Tiger"],["National fruit of India", "Banana", "Lichi", "Apple", "Mango", "Mango"]]
-let index = 0;
-let score = 0;
-let lastindex = arr.length;
+let arr = [["National Animal of India?", "Lion", "Tiger", "Horse", "Deer", "Tiger"],
+["National fruit of India", "Banana", "Lichi", "Apple", "Mango", "Mango"],
+["Prime Minister of india.", "Modi", "Rahul", "Kejriwal", "Lalu", "Modi"]]
 const Question = document.querySelector(".question")
 const options = document.querySelectorAll("li")
-
 const next = document.querySelector("button")
+const scr = document.querySelector(".Scr")
+const cont = document.querySelector(".qna-box")
 
+let index = 0;
+let score = 0;
 
-function blockoptions(){
-    options.forEach((op) =>{
+console.log(arr.length);
+
+function blockoptions() {
+    options.forEach((op) => {
         op.onclick = null;
     })
 }
 
 
-options.forEach((opt) =>{
-    opt.onclick = function (){
-        if(this.innerText === arr[index][5]){
-            this.classList.add('right')
-            blockoptions();
+function finalscore(){
+    cont.textContent=''
+}
+
+function checkans() {
+    options.forEach((opt) => {
+        opt.onclick = function () {
+            if (this.innerText === arr[index][5]) {
+                this.classList.add('right')
+                blockoptions();
+
+                score = score + 1;
+                scr.innerText = score
+
+                // if(index == arr.length-1){
+                //     console.log(index);
+                //     finalscore();
+            
+                // }
+            }
+
+
+            else if (this.innerText !== arr[index][5]) {
+                this.classList.add('wrong');
+                options.forEach((op) => {
+                    if (op.innerText === arr[index][5]) {
+                        op.classList.add('right')
+
+                    }
+                })
+                blockoptions();
+
+                // if(index == arr.length-1){
+                //     console.log(index);
+                //     dela
+                //     finalscore();
+            
+                // }
+            }
+
         }
-        
-
-        else if(this.innerText !== arr[index][5]){
-            this.classList.add('wrong');
-            options.forEach((op)=>{
-                if(op.innerText===arr[index][5]){
-                    op.classList.add('right')
-
-                }
-            })
-            blockoptions();
-        }
-
-    }
-    
-})
-
+    })
+}
 //display content
 
-function displayquestions(arr){
+checkans();
 
-    Question.innerText = arr[index][0];
-    let i=1;
-    options.forEach((opt) =>{
-        opt.innerText=arr[index][i]
+function displayquestions() {
+
+    Question.innerText = index + 1 + ". " + arr[index][0];
+    let i = 1;
+    options.forEach((opt) => {
+        opt.innerText = arr[index][i]
         i++;
     })
-    index++;
     console.log(Question);
-    console.log(arr[0][0]);
+    console.log([index][0]);
 
 }
 
-// displayquestions(arr)
+displayquestions();
 
-next.onclick = displayquestions(arr)
+
+next.addEventListener("click", function () {
+    if (index < arr.length-1) {
+        index++;
+        displayquestions();
+
+        options.forEach((op) => {
+            op.classList.remove('right', 'wrong')
+        })
+
+        checkans();
+    }
+
+
+})
